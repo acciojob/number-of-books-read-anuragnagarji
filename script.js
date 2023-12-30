e5b86e35f83a4420959f88022f50e82db8e79a3e
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-30 11:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,13 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  let count=0;
+      for(let i in library){
+          if(library[i].readingStatus==true){
+            count++;
+          }
+      }
+  return count;
 };
 
 // Do not change the code below
